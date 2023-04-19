c792fae00c7afaf030f0a9cd514f37f5e753f04a
Adham Omari <adhamomarics@gmail.com>
2023-04-20 00:30:05 +0300

    fix video

--- src/Component/BackgroundVideo/BackgroundVideo .jsx
+++ src/Component/BackgroundVideo/BackgroundVideo .jsx
@@ -5,10 +5,7 @@ const BackgroundVideo = () => {
   return (
     <div className='background-video'>
       <video autoPlay loop muted className='video'>
-        <source
-          src='https://www.shutterstock.com/shutterstock/videos/1086192224/preview/stock-footage-super-slow-motion-shot-of-wok-pan-with-flying-ingredients-in-the-air-and-fire-flames-filmed-on.webm'
-          type='video/mp4'
-        />
+        <source src='src/Food1.mp4' type='video/mp4' />
       </video>
     </div>
   )
