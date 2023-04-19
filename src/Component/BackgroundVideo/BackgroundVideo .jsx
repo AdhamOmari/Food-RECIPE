b7df9befb4761ef362136e7bca21ad51cad49062
Adham Omari <adhamomarics@gmail.com>
2023-04-20 00:33:35 +0300

    fix video

--- src/Component/BackgroundVideo/BackgroundVideo .jsx
+++ src/Component/BackgroundVideo/BackgroundVideo .jsx
@@ -5,7 +5,10 @@ const BackgroundVideo = () => {
   return (
     <div className='background-video'>
       <video autoPlay loop muted className='video'>
-        <source src='/src/Food1.mp4' type='video/mp4' />
+        <source
+          src='https://www.shutterstock.com/video/clip-1065498967-stir-fry-noodles-vegetables-flames-super-slow'
+          type='video/mp4'
+        />
       </video>
     </div>
   )
